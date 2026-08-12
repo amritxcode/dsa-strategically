f78a968ddbf7a2381d706e8930d312a6071f173e
Amrit Ray <amritray43@gmail.com>
2026-08-12 22:31:07 +0530

    feat: add hashing frequency for DSA

--- dsa_frequency_hashing.py
+++ dsa_frequency_hashing.py
@@ -2,15 +2,10 @@ def first_non_repeating(s):
     freq = {}
     for i in s:
         freq[i] = freq.get(i, 0) + 1
-
-    min_freq = float('inf')
-    answer = None
-
-    for key, value in freq.items():
-        if value < min_freq:
-            min_freq = value
-            answer = key
-
-    return answer
+    
+    for key in freq:
+        if freq[key] == 1:
+            return key
+        
 s = input()
 print(first_non_repeating(s))
