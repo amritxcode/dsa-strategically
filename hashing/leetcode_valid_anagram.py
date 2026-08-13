01c5eae9e4571d8b217258b81ffea8e6134da6cb
Amrit Ray <amritray43@gmail.com>
2026-08-13 06:23:07 +0530

    feat: add LeetCode 242 Valid Anagram

---
+++ hashing/leetcode_valid_anagram.py
@@ -0,0 +1,20 @@
+def is_anagram(s, t):
+    freq = {}
+
+    for i in s:
+        freq[i] = freq.get(i, 0) + 1
+
+    for i in t:
+        if i not in freq:
+            return False
+
+        freq[i] -= 1
+
+        if freq[i] < 0:
+            return False
+
+    return True
+
+s = input()
+t = input()
+print(is_anagram(s,t))
