b27b7f88b579cdd5738378aa0a3f7990743ac517
Amrit Ray <amritray43@gmail.com>
2026-09-03 11:42:55 +0530

    feat: add valid palindrome code

--- arrays/valid_palindrome.py
+++ arrays/valid_palindrome.py
@@ -4,10 +4,10 @@ class Solution():
         r = len(s) - 1
         while l < r:
             
-            while l < r and not s[l].isalpha():
+            while l < r and not s[l].isalnum():
                 l += 1
             
-            while l < r and not s[r].isalpha():
+            while l < r and not s[r].isalnum():
                 r -= 1
             if s[l].lower() != s[r].lower():
                 return False
